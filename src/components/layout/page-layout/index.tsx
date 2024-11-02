07b061cdd4c21088d8223be80576a8c8a5db1fe2
lezgea <muslim.ragimov@yahoo.com>
2024-11-02 14:31:32 -0400

    Empty h1 tags have been hidden

--- src/components/layout/page-layout/index.tsx
+++ src/components/layout/page-layout/index.tsx
@@ -15,7 +15,10 @@ const PageLayout: FC<IPageLayout> = ({
         <div className="min-h-screen flex flex-col py-[50px] lg:py-10">
             <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
             <main id="main-content" className="w-full">
-                <h1 className="sr-only">{title}</h1>
+                {
+                    !!title &&
+                    <h1 className="sr-only">{title}</h1>
+                }
                 {children}
             </main>
         </div>
