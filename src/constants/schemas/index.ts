08e01f7cebaad993b474fcc424cfe9e45e7c32f3
lezgea <muslim.ragimov@yahoo.com>
2024-11-13 15:02:35 -0500

    Images schema markups have been added

--- src/constants/schemas/index.ts
+++ src/constants/schemas/index.ts
@@ -14,4 +14,101 @@ export const SCHEMA_IMAGES = [
         url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Flogos%2Ful-google.webp&w=828&q=75',
         description: "Google company website icon",
     },
+    {
+        name: 'Better Business Bureau Icon',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Flogos%2Ful-bbb.webp&w=640&q=75',
+        description: "Better Business Bureau company website icon",
+    },
+    {
+        name: 'Yelp Icon',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Flogos%2Ful-yelp.webp&w=640&q=75',
+        description: "Yelp company website icon",
+    },
+    {
+        name: 'Thumbtack Icon',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Flogos%2Ful-thumbtack.webp&w=640&q=75',
+        description: "Thubtack company website icon",
+    },
+    {
+        name: 'Trustpilot Icon',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Flogos%2Ful-trustpilot.webp&w=640&q=75',
+        description: "Trustpilot company website icon",
+    },
+
+    // SERVICE IMAGES
+    {
+        name: 'Residential Refrigerator Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Frefrigerator.webp&w=640&q=75',
+        description: "Image of a Residential Refrigerator representing UltraFix's Residential Refrigerator repair services",
+    },
+    {
+        name: 'Residential Ice Machine Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fice_machine.webp&w=640&q=75',
+        description: "Image of a Residential Ice Machine representing UltraFix's Residential Ice Machine repair services",
+    },
+    {
+        name: 'Residential Washer Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fwasher.webp&w=640&q=75',
+        description: "Image of a Residential Washer representing UltraFix's Residential Washer repair services",
+    },
+    {
+        name: 'Residential Dryer Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fdryer.webp&w=640&q=75',
+        description: "Image of a Residential Dryer representing UltraFix's Residential Dryer repair services",
+    },
+    {
+        name: 'Residential Dishwasher Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fdishwasher.webp&w=640&q=75',
+        description: "Image of a Residential Dishwasher representing UltraFix's Residential Dishwasher repair services",
+    },
+    {
+        name: 'Residential Oven Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Foven.webp&w=640&q=75',
+        description: "Image of a Residential Oven representing UltraFix's Residential Oven repair services",
+    },
+    {
+        name: 'Residential Cooktop Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fcooktop.webp&w=640&q=75',
+        description: "Image of a Residential Cooktop representing UltraFix's Residential Cooktop repair services",
+    },
+    {
+        name: 'Residential Microwave Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fmicrowave.webp&w=640&q=75',
+        description: "Image of a Residential Microwave representing UltraFix's Residential Microwave repair services",
+    },
+    {
+        name: 'Residential Wine Cooler Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fwine_cooler.webp&w=640&q=75',
+        description: "Image of a Residential Wine Cooler representing UltraFix's Residential Wine Cooler repair services",
+    },
+    {
+        name: 'Residential Freezer Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Ffreezer.webp&w=640&q=75',
+        description: "Image of a Residential Freezer representing UltraFix's Residential Freezer repair services",
+    },
+    {
+        name: 'Residential Vent Hood Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fvent_hood.webp&w=640&q=75',
+        description: "Image of a Residential Vent Hood representing UltraFix's Residential Vent Hood repair services",
+    },
+    {
+        name: 'Residential Garbage Disposal Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fresidential%2Fgarbage_disposal.webp&w=640&q=75',
+        description: "Image of a Residential Garbage Disposal representing UltraFix's Residential Garbage Disposal repair services",
+    },
+    {
+        name: 'Commercial Refrigerator Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fcommercial%2Frefrigerator.webp&w=640&q=75',
+        description: "Image of a Commercial Refrigerator representing UltraFix's Commercial Refrigerator repair services",
+    },
+    {
+        name: 'Commercial Ice Machine Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fcommercial%2Fice_machine.webp&w=640&q=75',
+        description: "Image of a Commercial Ice Machine representing UltraFix's Commercial Ice Machine repair services",
+    },
+    {
+        name: 'Commercial Freezer Image',
+        url: 'https://ultrafix.com/_next/image?url=%2Fimg%2Fservices%2Fcommercial%2Ffreezer.webp&w=640&q=75',
+        description: "Image of a Commercial Freezer representing UltraFix's Commercial Freezer repair services",
+    },
 ]
