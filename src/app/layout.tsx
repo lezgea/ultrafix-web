f3e7b52cc4bf2ce24603c14cb1ee17f61a3cfda7
lezgea <muslim.ragimov@yahoo.com>
2024-11-22 20:50:14 -0500

    Google tag manager scripts have been added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,20 @@ export default function RootLayout({
         {/* Link to the favicon */}
         <link rel="icon" href="/favicon.ico" />
         {/* <link rel="preload" href="/img/houston_car.webp" as="image" /> */}
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KLKNJZX6');
+            `,
+          }}
+        />
+
         {/* JSON-LD Schema Markup for Organization */}
         <script
           type="application/ld+json"
@@ -249,6 +263,15 @@ export default function RootLayout({
         />
       </head>
       <body className={`${poppins.className}`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLKNJZX6"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ReduxProvider>
           <ToastProvider>
             <Header />
