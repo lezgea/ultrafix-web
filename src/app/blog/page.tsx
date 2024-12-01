9fdba1ee19b571dde2b3be3dc920a2f7879a2966
lezgea <muslim.ragimov@yahoo.com>
2024-11-30 20:51:50 -0500

    Added all sections to the blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Metadata } from 'next';
 import PageLayout from '@components/layout/page-layout';
-import { BlogSection } from '@components/features';
+import { AboutUsSection, BlogSection, BrandsSection, CommercialServicesSection, ContactSection, LocationsSection, ResidentialServicesSection, WhyUsSection } from '@components/features';
 import Image from 'next/image';
 
 
@@ -17,17 +17,14 @@ const Blog: React.FC = () => {
             largeYPadding
             title="The number one Appliance Repair service in US"
         >
-            {/* <Image
-                src="/img/blog/blog_banner.webp"
-                width={1200} // Adjust width and height based on your layout
-                height={400}
-                className="w-full h-auto my-6"
-                alt={"Blog banner"}
-                loading="lazy"
-                sizes="(max-width: 1200px) 100vw, 100vw"
-            /> */}
-
             <BlogSection />
+            <ResidentialServicesSection />
+            <CommercialServicesSection />
+            <ContactSection />
+            <LocationsSection />
+            <AboutUsSection />
+            <BrandsSection />
+            <WhyUsSection />
         </PageLayout>
     );
 };
