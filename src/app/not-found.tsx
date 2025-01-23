7b14f3bace281c731c880ac0e9c8fde0e78027c2
lezgea <muslim.ragimov@yahoo.com>
2025-01-23 15:31:42 -0500

    Error pages for not existing links / UI with home button

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,17 @@
-"use client";
+import Link from 'next/link';
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
-import { Loader } from '@components/shared';
-
-
-const NotFoundPage = () => {
-    const router = useRouter();
-
-    // React.useEffect(() => {
-    //     router.replace('/');
-    // }, [router]);
-
-    return <Loader />;
+const NotFound = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">404 - Page Not Found</h1>
+            <p className="text-lg text-gray-600 mb-6">
+                The page you’re looking for doesn’t exist.
+            </p>
+            <Link href="/" className="px-6 py-3 bg-primary text-white rounded-md hover:bg-primaryDark">
+                Return to Home
+            </Link>
+        </div>
+    );
 };
 
-export default NotFoundPage;
+export default NotFound;
