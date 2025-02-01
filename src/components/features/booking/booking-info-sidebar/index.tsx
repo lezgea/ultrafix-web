60002afc34626ccc3a988b35c4560ac0774a1ef8
lezgea <muslim.ragimov@yahoo.com>
2025-02-01 03:02:39 -0500

    mobile view Boking Info modal button has been hidden

--- src/components/features/booking/booking-info-sidebar/index.tsx
+++ src/components/features/booking/booking-info-sidebar/index.tsx
@@ -20,8 +20,7 @@ export const BookingInfoSidebar: React.FC<IBookingInfoSidebar> = (props) => {
 
     return (
         <div>
-            <div className='flex z-30 md:hidden fixed items-center h-screen'>
-                {/* <div className='absolute flex items-center justify-center text-sm text-white bg-primaryLight z-10 w-5 h-5 rounded-full right-0'>3</div> */}
+            {/* <div className='flex z-30 md:hidden fixed items-center h-screen'>
                 <button
                     type="button"
                     className="inline-flex w-auto text-center text-lg items-center px-5 pt-2 pb-5 text-white transition-all bg-primaryMedium rounded-lg sm:w-auto hover:bg-primaryDark shadow-neutral-300 hover:shadow-lg hover:shadow-neutral-300 hover:-translate-y-px focus:shadow-none rotate-90"
@@ -29,7 +28,7 @@ export const BookingInfoSidebar: React.FC<IBookingInfoSidebar> = (props) => {
                 >
                     Booking Info
                 </button>
-            </div>
+            </div> */}
 
             <div className='fixed z-20 flex flex-col h-screen pt-[50px] px-[20px] pb-[60px]'>
                 {
