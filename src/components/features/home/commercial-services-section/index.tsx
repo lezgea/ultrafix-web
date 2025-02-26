998f86aee1343401a4a45b85d14fbc2f7da457eb
lezgea <muslim.ragimov@yahoo.com>
2025-02-25 23:41:20 -0500

    Service section title updates have been reverted

--- src/components/features/home/commercial-services-section/index.tsx
+++ src/components/features/home/commercial-services-section/index.tsx
@@ -18,7 +18,7 @@ export const CommercialServicesSection: React.FC = () => {
     return (
         <SectionLayout
             scrollId="com_services"
-            title={brandInfo?.text ? `Our Commercial <span style="color:#2b7de2">${brandInfo?.text}</span> Services in ${cityInfo?.title}, ${cityInfo?.state_short}` : "Our Commercial Services"}
+            title={brandInfo?.text ? `Our Commercial <span style="color:#2b7de2">${brandInfo?.text}</span> Services` : "Our Commercial Services"}
             description="We have been providing top service! See just how our UltraFix Appliance Repair Service can better your life today!"
         >
             <div className='flex flex-wrap gap-5 md:gap-10 items-center justify-center'>
--- src/components/features/home/residential-services-section/index.tsx
+++ src/components/features/home/residential-services-section/index.tsx
@@ -18,7 +18,7 @@ export const ResidentialServicesSection: React.FC = () => {
     return (
         <SectionLayout
             scrollId="res_services"
-            title={brandInfo?.text ? `Our Residential <span style="color:#2b7de2">${brandInfo?.text}</span> Services in ${cityInfo?.title}, ${cityInfo?.state_short}` : "Our Residential Services"}
+            title={brandInfo?.text ? `Our Residential <span style="color:#2b7de2">${brandInfo?.text}</span> Services` : "Our Residential Services"}
             description="We have been providing top service! See just how our UltraFix Appliance Repair Service can better your life today!"
         >
             <div className='flex flex-wrap gap-5 md:gap-10 items-center justify-center'>
