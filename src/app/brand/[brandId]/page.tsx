460c4815b80bbf87d5edfe1ff74d1c3846f80811
lezgea <muslim.ragimov@yahoo.com>
2025-02-27 16:12:28 -0500

    Main Brand's page metadata

--- src/app/brand/[brandId]/page.tsx
+++ src/app/brand/[brandId]/page.tsx
@@ -13,89 +13,90 @@ interface IServiceProps {
 }
 
 
-// export async function generateMetadata({ params }: IServiceProps) {
-//     const { brandId } = params;
+export async function generateMetadata({ params }: IServiceProps) {
+    const { brandId } = params;
 
-//     const serviceKey = service as keyof typeof RESIDENTIAL_SERVICES;
+    const brandInfo = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/brands/${brandId}`)
+        .then((res) => res.json());
 
-//     const title = serviceKey
-//         ? `Residential ${RESIDENTIAL_SERVICES[serviceKey].title} repair | UltraFix®`
-//         : 'Residential Appliance Repair Services | UltraFix®';
-//     const description = serviceKey
-//         ? `Get Professional ${RESIDENTIAL_SERVICES[serviceKey].title} repair by Ultrafix®. Same-Day Service, Certified Technicians, and High-Quality Parts for All Your Appliance Repair Needs.`
-//         : 'UltraFix offers trusted appliance repair services across the United States.';
+    const title = brandInfo?.data
+        ? `${brandInfo?.data?.text} Appliance Repair Services | UltraFix®`
+        : 'Appliance Repair Services | UltraFix®';
+    const description = brandInfo?.data
+        ? `UltraFix offers trusted ${brandInfo?.data?.text} appliance repair services. Fast, same-day repairs for refrigerators, washers, dryers, and more!`
+        : 'UltraFix offers trusted appliance repair services across the United States.';
 
-//     return {
-//         title,
-//         description,
-//         keywords: [
-//             'Appliance Repair',
-//             `Appliance Repair Service`,
-//             `${RESIDENTIAL_SERVICES[serviceKey].title} Repair Service`,
-//             `Best ${RESIDENTIAL_SERVICES[serviceKey].title} Repair Service`,
-//             `Residential ${RESIDENTIAL_SERVICES[serviceKey].title} Repair`,
-//             ...RESIDENTIAL_SERVICES[serviceKey].keywords,
-//             'appliance repair near me',
-//             'appliance repair',
-//             'appliance repair service',
-//             'commercial appliance repair',
-//             'repair appliances',
-//             'service appliance repair',
-//             'Appliance Repair',
-//             'Local Appliance Repair',
-//             'Emergency Appliance Repair',
-//             'Same Day Appliance Repair',
-//             'Affordable Appliance Repair',
-//             'Certified Appliance Technicians',
-//             'Refrigerator Repair Houston',
-//             'Dishwasher Repair Houston',
-//             'Oven and Stove Repair',
-//             'Microwave Repair',
-//             'Freezer and Ice Maker Repair',
-//             'Garbage Disposal Repair',
-//             'Washer and Dryer Repair',
-//             'Major Appliance Repair',
-//             'Commercial Appliance Repair',
-//             'Houston Appliance Service',
-//             'Best Appliance Repair in',
-//             'Nearby Appliance Repair',
-//         ],
-//         robots: {
-//             index: true,
-//             follow: true,
-//             nocache: false,
-//             googleBot: {
-//                 index: true,
-//                 follow: true,
-//                 noarchive: false,
-//             },
-//         },
-//         openGraph: {
-//             title,
-//             description,
-//             url: `https://ultrafix.com/appliance-services/residential/${RESIDENTIAL_SERVICES[serviceKey].link}`,
-//             images: [
-//                 {
-//                     url: `https://ultrafix.com/img/services/residential/${RESIDENTIAL_SERVICES[serviceKey].value}.webp`,
-//                     width: 1200,
-//                     height: 630,
-//                     alt: `Residential ${RESIDENTIAL_SERVICES[serviceKey].title} Repair Service`,
-//                 },
-//             ],
-//             locale: 'en_US',
-//             type: 'website',
-//         },
-//         twitter: {
-//             card: 'summary_large_image',
-//             title,
-//             description,
-//             images: [`https://ultrafix.com/img/services/residential/${RESIDENTIAL_SERVICES[serviceKey].value}.webp`],
-//         },
-//         alternates: {
-//             canonical: `https://ultrafix.com/appliance-services/residential/${RESIDENTIAL_SERVICES[serviceKey].link}`,
-//         },
-//     };
-// }
+    return {
+        title,
+        description,
+        keywords: [
+            'Appliance Repair',
+            `Appliance Repair Service`,
+            `${brandInfo?.data?.text} Apppliance Repair Service`,
+            `Best ${brandInfo?.data?.text} Appliance Repair Service`,
+            `Residential ${brandInfo?.data?.text} Repair`,
+            brandInfo?.data?.text,
+            'appliance repair near me',
+            'appliance repair',
+            'appliance repair service',
+            'commercial appliance repair',
+            'repair appliances',
+            'service appliance repair',
+            'Appliance Repair',
+            'Local Appliance Repair',
+            'Emergency Appliance Repair',
+            'Same Day Appliance Repair',
+            'Affordable Appliance Repair',
+            'Certified Appliance Technicians',
+            'Refrigerator Repair Houston',
+            'Dishwasher Repair Houston',
+            'Oven and Stove Repair',
+            'Microwave Repair',
+            'Freezer and Ice Maker Repair',
+            'Garbage Disposal Repair',
+            'Washer and Dryer Repair',
+            'Major Appliance Repair',
+            'Commercial Appliance Repair',
+            'Houston Appliance Service',
+            'Best Appliance Repair in',
+            'Nearby Appliance Repair',
+        ],
+        robots: {
+            index: true,
+            follow: true,
+            nocache: false,
+            googleBot: {
+                index: true,
+                follow: true,
+                noarchive: false,
+            },
+        },
+        openGraph: {
+            title,
+            description,
+            url: `https://ultrafix.com/brand/${brandId}`,
+            images: [
+                {
+                    url: brandInfo?.data?.logo?.url,
+                    width: 1200,
+                    height: 630,
+                    alt: `${brandInfo?.data?.title} Applience Repair Service`,
+                },
+            ],
+            locale: 'en_US',
+            type: 'website',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title,
+            description,
+            images: [brandInfo?.data?.logo?.url],
+        },
+        alternates: {
+            canonical: `https://ultrafix.com/brand/${brandId}`,
+        },
+    };
+}
 
 
 const BrandInfoPage: React.FC<IServiceProps> = ({ params }) => {
